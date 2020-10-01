f4a6de65d29bc8ae89f448acd79175f20cad2079
agevork <agevork@yandex.ru>
2020-10-01 09:41:46 +0300

    01.10

--- src/Cli.php
+++ src/Cli.php
@@ -5,12 +5,12 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
+/** cпрашивает имя и здоровается */
 function run() 
 {
-
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
 
-?>
+
